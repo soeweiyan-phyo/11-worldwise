ad48211ed52a0a75cc20458ce63edb022ad68ae9
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-08-23 16:57:15 +1000

    Create main pages and simple routes

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
