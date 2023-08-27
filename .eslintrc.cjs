eb31aaf5ed534e031592c89d2090461d297a038b
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-08-27 19:07:27 +1000

    Implementing the cities list

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "react/prop-types": "off",
+    "no-unused-vars": ["warn", { varsIgnorePattern: "VARIABLE_NAME" }],
   },
 };
